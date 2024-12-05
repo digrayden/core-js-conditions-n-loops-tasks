8e7cc0fba967e6724c3d9d494d70ff97cf8c9ac7
Diana Grayden <graydendi@mail.ru>
2024-12-05 16:40:54 +0300

    feat: add and fix solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -154,8 +154,32 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  const digitWords = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    '-': 'minus',
+    '.': 'point',
+    ',': 'point',
+  };
+
+  const result = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const char = numberStr[i];
+    if (Object.prototype.hasOwnProperty.call(digitWords, char)) {
+      result.push(digitWords[char]);
+    }
+  }
+
+  return result.join(' ');
 }
 
 /**
